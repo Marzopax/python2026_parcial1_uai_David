997963e76c28c710b6452d103acb41a8e7a3aeda
Leandrojmontenegro@gmail.com <Leandrojmontenegro@gmail.com>
2026-06-19 18:48:02 -0300

    fix text

--- LeandroMontenegro/ejercicio_2.py
+++ LeandroMontenegro/ejercicio_2.py
@@ -4,8 +4,8 @@ def pedir_entero():
 
         while True:
             try:
-                entero = int(input("ingresa un entero: "))
-                print(f"El número ingresado es: {entero}")
+                entero = int(input("ingresa edad: "))
+                print(f"la edad ingresada es: {entero}")
                 break;
             except ValueError:
                 print("Error: ingresa un número válido.")
