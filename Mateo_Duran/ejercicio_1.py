481fd8abc4e0b2ce3d7cff4df04f2a305a940576
Marzopax <150204301+Marzopax@users.noreply.github.com>
2026-07-17 17:57:35 -0300

    Delete Mateo_Duran directory

--- Mateo_Duran/ejercicio_1.py
+++
@@ -1,5 +0,0 @@
-nombre = input ("Ingrese su nombre: ")
-
-nombre_final = nombre.strip().title()
-
-print(f"Hola {nombre_final}")
--- Mateo_Duran/ejercicio_7.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if len(sys.argv) != 2:
-    sys.exit("Error: Se esperaba exactamente un argumento adicional.")
-
-argumento_adicional = sys.argv[1]
-
-print(f"¡Todo correcto! El argumento ingresado es: {argumento_adicional}")
