b1b8767fe9850cf9c150d0646dfc0381c4215780
Marzopax <150204301+Marzopax@users.noreply.github.com>
2026-07-17 17:56:10 -0300

    Delete federico_riquelme directory

--- federico_riquelme/test.py
+++
@@ -1,6 +0,0 @@
-from ejercicio_10 import calcular_cuadrado
-
-def test_calcular_cuadrado():
-    assert calcular_cuadrado(5) == 25
-    assert calcular_cuadrado(0) == 0
-    assert calcular_cuadrado(-3) == 9
